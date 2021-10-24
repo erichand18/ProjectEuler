302f4a75373cca1b371c2fdaa2a9362b19b9ed43
Eric Handley <ehandley96@gmail.com>
2021-10-24 03:57:05 -0500

    Added solutions for problems 10, 12, 13, and 14.

---
+++ src/Problem_10/problem10.ts
@@ -0,0 +1,36 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+/*
+By checking if each number between 3 and the target number is prime, we can simply find primes until 
+we reach two million, adding to a running sum while we do so.
+*/
+
+
+function sumPrimes(maxNum: number): number {
+  let sum: number = 2; //start at 2 to cover only even prime
+
+  let primeNumFlag: boolean = true; //boolean flag for if a num is prime
+
+  for (let i = 3; i < maxNum; i += 2) {
+    for (let p = 2; p <= Math.sqrt(i); p++) {
+      if (i % p === 0) {
+        primeNumFlag = false;
+      }
+    }
+
+    if (primeNumFlag === true) {
+      sum += i;
+    }
+
+    primeNumFlag = true;
+  }
+
+  return sum;
+
+}
+
+console.log(sumPrimes(2000000));
---
+++ src/Problem_14/problem14.js
@@ -0,0 +1,48 @@
+/*
+
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+*/
+/*
+The solution is fairly straightforward: iterate from 2 to one million and calculate the length of each COllatz chain.
+Once a longer chain is found, note which starting number produced it store the value. Return the longest chain at the end.
+*/
+function collatzLength(startingNum) {
+    var chainLength = 1;
+    while (startingNum !== 1) {
+        if (startingNum % 2 === 0) {
+            startingNum /= 2;
+        }
+        else {
+            startingNum = startingNum * 3 + 1;
+        }
+        chainLength++;
+    }
+    return chainLength;
+}
+function largestCollatzChainNum() {
+    var currentChain = 0;
+    var longestChain = 0;
+    var longestChainStartNum = 1;
+    for (var i = 2; i < 1000000; i++) {
+        currentChain = collatzLength(i);
+        if (currentChain > longestChain) {
+            longestChainStartNum = i;
+            longestChain = currentChain;
+        }
+    }
+    return longestChainStartNum;
+}
+console.log(largestCollatzChainNum());
